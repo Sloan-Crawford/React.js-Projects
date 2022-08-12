0e519bae578947f83f4a4cd02d765943a3104d10
Sloan-Crawford <sloan.crawford@gmail.com>
2022-08-12 12:31:16 -0400

    import react and babel script tags

---
+++ Guide-and-8-beginner-projects/1-static-info-site/index.js
@@ -0,0 +1,2 @@
+// syntax: React.DOM.render(what, where)
+ReactDOM.render(<h1>hey chums</h1>, document.getElementById("root"));
---
+++ Notes.js
@@ -0,0 +1,14 @@
+// ================ React.js complete notes ===================
+
+// React is an open source JavaScript library used for building user interfaces. 
+// It is currently maintained by Facebook, Instagram and a large community of developers. 
+// also has a sister library (React Native): provides architecture to native iOS & Android applications.
+
+// setting up (easy way):
+// first, get React and Babel cdn script tags (React for development to allow for debugging, babel.min).
+// JSX preprocessor - adding type="text/babel" to the index.js script. NOT SUITABLE FOR PRODUCTION.
+
+// start using react right away with:
+// ReactDOM (global object)
+// render() (method)
+// syntax: React.DOM.render(what, where)
