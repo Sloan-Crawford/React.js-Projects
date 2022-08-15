e569a69c74b325577c615015c0db6ed521eb2d59
Sloan-Crawford <sloan.crawford@gmail.com>
2022-08-15 11:12:43 -0400

    Added getting started section

--- Notes.js
+++ Notes.js
@@ -8,7 +8,26 @@
 // first, get React and Babel cdn script tags (React for development to allow for debugging, babel.min).
 // JSX preprocessor - adding type="text/babel" to the index.js script. NOT SUITABLE FOR PRODUCTION.
 
+// setting up (BETTER way):
+// use a good package manager (npm) and use Node.js (a good environment for developing server-side apps)
+// Create React App. In terminal navigate to folder you want to create a new react project folder:
+// npx create-react-app name-of-folder
+// cd
+
+
 // start using react right away with:
 // ReactDOM (global object)
 // render() (method)
-// syntax: React.DOM.render(what, where)
+// syntax: React.DOM.render(what html, where)
+
+// Why use React?
+// 1. It allows us to write COMPOSABLE code, which is more maintainable and flexible.
+// 2. It is DECLARATIVE - give general instructions (as opposed to specific, Imperative like in Vanilla JS)
+// (we rely on React to figure out how to turn what we have declaratively written into JS)
+// 
+
+// ----------- JSX ------------
+// JSX (javascript xml) = a flavour of js that looks a lot like html.
+// what React is actually creating with JSX are js objects that describe the element to put on the page.
+// JSX is like a function that returns us objects that react interprets to create elements.
+// with JSX we need to make sure we are returning a single parent element.
